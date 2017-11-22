ba4c1b978f6822405e2b68c5d1a83279cca52e50
Trollicorn <muddin3@stuy.edu>
2017-11-22 14:47:49 -0500

    Added methods to ReverseCharSequence

--- 04ReverseCharSequence/ReverseCharSequence.java
+++ 04ReverseCharSequence/ReverseCharSequence.java
@@ -1,4 +1,37 @@
 public class ReverseCharSequence implements CharSequence{
 
+    private String reverse;
+    private String original; 
 
+    public ReverseCharSequence(String str){
+	original = str; 
+	reverse = "";
+	for (int i = 0; i < str.length(); i++){
+	    reverse += str.charAt(i);
+	}
+    }
+
+    public char charAt(int index){
+	char ok = 'a';
+	for (int i = 0; i < reverse.length(); i++){
+	    if (i == index){
+		ok = reverse.charAt(i);
+	    }
+	}
+	return ok;
+    }
+
+    public int length(){
+	return reverse.length();
+    }
+    
+    public ReverseCharSequence subSequence(int start, int end){
+	ReverseCharSequence gather = new  ReverseCharSequence( original.substring(start,end));
+	return gather;
+    }
+
+    public String toString(){
+	return reverse;
+    }
+    
 }
