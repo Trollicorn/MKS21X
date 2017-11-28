b1dff6a1bd25825f2528deb778da363da957e2c7
Trollicorn <muddin3@stuy.edu>
2017-11-28 14:43:27 -0500

    Fixed Book.java bugs and finished LibraryBook.java

--- 05Library/Book.java
+++ 05Library/Book.java
@@ -25,15 +25,15 @@ public class Book{
 	return ISBN;
     }
 
-    public void setAuthor(){
+    public void setAuthor(String author){
 	this.author = author;
     }
 
-    public void setTitle(){
+    public void setTitle(String title){
 	this.title = title;
     }
 
-    public void setISBN(){
+    public void setISBN(String ISBN){
 	this.ISBN = ISBN;
     }
 
