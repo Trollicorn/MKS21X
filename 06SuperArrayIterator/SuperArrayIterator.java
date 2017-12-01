322be58a22eb706535835a37ed09981bd50ff607
Mohammed Uddin <muddin3@stuy.edu>
2017-12-01 00:42:24 -0500

    Finished SuperArrayIterator.java

--- 06SuperArrayIterator/SuperArrayIterator.java
+++ 06SuperArrayIterator/SuperArrayIterator.java
@@ -1,4 +1,22 @@
+import java.util.Iterator; 
 public class SuperArrayIterator implements Iterator<String>{
+	private SuperArray array; 
+	private int current; 
 
-	
+	public SuperArrayIterator(SuperArray array){
+		this.array = array; 
+		current = 0; 
+	}
+
+	public boolean hasNext(){
+		return current < array.size();
+	}
+
+	public String next(){
+		current++; 
+		return array.get(current - 1);
+	}
+
+
+
 }
