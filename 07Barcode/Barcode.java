842e2669baa022a055c8b79b927e3b68b201a12d
Trollicorn <muddin3@stuy.edu>
2017-12-04 16:57:03 -0500

    finished (hopefully)

--- 07Barcode/Barcode.java
+++ 07Barcode/Barcode.java
@@ -29,6 +29,15 @@ public class Barcode implements Comparable<Barcode>{
     private String numToBar(int num){
 	return guide[num];
     }
+
+    private int barToNum(String bar){
+	for (int i = 0; i < 10; i++){
+	    if (bar.equals(guide[i])){
+		return i;
+	    }
+	}
+	return -1;
+    }
     
     public String getZip(){
 	return zip;
@@ -59,8 +68,24 @@ public class Barcode implements Comparable<Barcode>{
 	return temp.getCode();
     }
 
-    //public static String toZip(String code){
-	
-    //    }
+    public String toZip(String code){
+	if (code.length() != 32 || code.charAt(0) != '|' || code.charAt(31) != '|'){
+	    throw new IllegalArgumentException();
+	}
+	code = code.substring(1,31);
+	String zip = "";
+	for (int i = 0; i < 30; i+=5){
+	    int temp = barToNum(code.substring(i,i+5));
+	    if (temp == -1){
+		throw new IllegalArgumentException();
+	    }
+	    zip += temp;
+	}
+	int checkSum = Integer.parseInt(zip.substring(5,6));
+	if (checkSum != checkSum(zip.substring(0,5))){
+	    throw new IllegalArgumentException();
+	}
+	return zip;
+    }
     
 }
