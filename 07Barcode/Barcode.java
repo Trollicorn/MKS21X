7e219f8be2c4c9f4988218ced25ca9de42f8dbd5
Trollicorn <muddin3@stuy.edu>
2017-12-04 16:09:38 -0500

    made Barcode.java implement Comparable<Barcode> and editted some methods

--- 07Barcode/Barcode.java
+++ 07Barcode/Barcode.java
@@ -1,39 +1,46 @@
-public class Barcode{
-	private String zip;
-	private String[] guide; 
-	
-	public Barcode(String zip){
-		this.zip = zip + checkSum(zip); 
-		guide = new String[] {"||:::",":::||","::|:|","::||:",":|::|",":|:|:",":||::","|:::|","|::|:","|:|::" };
+public class Barcode implements Comparable<Barcode>{
+    private String zip;
+    private String[] guide; 
+    
+    public Barcode(String zip){
+	this.zip = zip; 
+	guide = new String[] {"||:::",":::||","::|:|","::||:",":|::|",":|:|:",":||::","|:::|","|::|:","|:|::" };
+    }
+    
+    private int checkSum(String zip){
+	int sum = 0;
+	for (int i = 0; i < zip.length(); i++){
+	    sum += Integer.parseInt(zip.substring(i,i+1));
 	}
-
-	public int checkSum(String zip){
-		int sum = 0;
-		for (int i = 0; i < zip.length(); i++){
-			sum += Integer.parseInt(zip.substring(i,i+1));
-		}
-		return sum;
+	return sum;
+    }
+    
+    private String numToBar(int num){
+	return guide[num];
+    }
+    
+    public String getZip(){
+	return zip;
+    }
+    
+    public String getCode(){
+	String code = "|";
+	for (int i = 0; i < 5; i++){
+	    code += numToBar( Integer.parseInt( zip.substring(i,i+1) )  );
+	    if (i == 4){
+		code += numToBar (checkSum(zip));
+	    }
 	}
-
-	private String numToBar(int num){
-		return guide[num];
-	}
-
-	public String getZip(){
-		return zip;
-	}
-
-	public String getCode(){
-		String code = "|";
-		for (int i = 0; i < zip.length(); i++){
-			code += numToBar( Integer.parseInt( zip.substring(i,i+1) )  );
-		}
-		code += "|";
-		return code; 
-	}
-
-	public String toString(){
-		return getCode();
-	}
-
+	code += "|";
+	return code; 
+    }
+    
+    public String toString(){
+	return getCode();
+    }
+    
+    public int compareTo(Barcode other){
+	return Integer.parseInt(getZip()) - Integer.parseInt(other.getZip());
+    }
+    
 }
