7e2ae088905c3617a073711f0fe89ffd889be07b
Mohammed Uddin <muddin3@stuy.edu>
2018-01-06 21:01:48 -0500

    renamed lab 04's directory and made toZip in lab 07 static

--- 07Barcode/Barcode.java
+++ 07Barcode/Barcode.java
@@ -5,87 +5,100 @@ public class Barcode implements Comparable<Barcode>{
     private String[] guide; 
     
     public Barcode(String zip){
-	if (zip.length() != 5 ){
-	    throw new IllegalArgumentException();
-	}
-	try {
-	    Integer.parseInt(zip);
-	}
-	catch (NumberFormatException e){
-	    throw new IllegalArgumentException();
-	}
-	this.zip = zip; 
-	guide = new String[] {"||:::",":::||","::|:|","::||:",":|::|",":|:|:",":||::","|:::|","|::|:","|:|::" };
+		if (zip.length() != 5 ){
+		    throw new IllegalArgumentException();
+		}
+		try {
+		    Integer.parseInt(zip);
+		}
+		catch (NumberFormatException e){
+		    throw new IllegalArgumentException();
+		}
+		this.zip = zip; 
+		guide = new String[] {"||:::",":::||","::|:|","::||:",":|::|",":|:|:",":||::","|:::|","|::|:","|:|::" };
+    }
+
+ 
+    private static String[] guide(){
+    	String[] guide = new String[] {"||:::",":::||","::|:|","::||:",":|::|",":|:|:",":||::","|:::|","|::|:","|:|::" };
+    	return guide;
     }
     
     private int checkSum(String zip){
-	int sum = 0;
-	for (int i = 0; i < zip.length(); i++){
-	    sum += Integer.parseInt(zip.substring(i,i+1));
-	}
-	return sum;
+		int sum = 0;
+		for (int i = 0; i < zip.length(); i++){
+		    sum += Integer.parseInt(zip.substring(i,i+1));
+		}
+		return sum;
     }
     
     private String numToBar(int num){
-	return guide[num];
+		return guide[num];
     }
 
-    private int barToNum(String bar){
-	for (int i = 0; i < 10; i++){
-	    if (bar.equals(guide[i])){
-		return i;
-	    }
-	}
-	return -1;
+    private static int barToNum(String bar){
+		for (int i = 0; i < 10; i++){
+		    if (bar.equals(guide()[i])){
+				return i;
+		    }
+		}
+		return -1;
     }
     
     public String getZip(){
-	return zip;
+		return zip;
     }
     
     public String getCode(){
-	String code = "|";
-	for (int i = 0; i < 5; i++){
-	    code += numToBar( Integer.parseInt( zip.substring(i,i+1) )  );
-	    if (i == 4){
-		code += numToBar (checkSum(zip));
-	    }
-	}
-	code += "|";
-	return code; 
+		String code = "|";
+		for (int i = 0; i < 5; i++){
+		    code += numToBar( Integer.parseInt( zip.substring(i,i+1) )  );
+		    if (i == 4){
+				code += numToBar (checkSum(zip));
+		    }
+		}
+		code += "|";
+		return code; 
     }
     
     public String toString(){
-	return getCode();
+		return getCode();
     }
     
     public int compareTo(Barcode other){
-	return Integer.parseInt(getZip()) - Integer.parseInt(other.getZip());
+		return Integer.parseInt(getZip()) - Integer.parseInt(other.getZip());
     }
 
     public static String toCode(String zip){
-	Barcode temp = new Barcode(zip);
-	return temp.getCode();
+		Barcode temp = new Barcode(zip);
+		return temp.getCode();
     }
 
-    public String toZip(String code){
-	if (code.length() != 32 || code.charAt(0) != '|' || code.charAt(31) != '|'){
-	    throw new IllegalArgumentException();
-	}
-	code = code.substring(1,31);
-	String zip = "";
-	for (int i = 0; i < 30; i+=5){
-	    int temp = barToNum(code.substring(i,i+5));
-	    if (temp == -1){
-		throw new IllegalArgumentException();
-	    }
-	    zip += temp;
-	}
-	int checkSum = Integer.parseInt(zip.substring(5,6));
-	if (checkSum != checkSum(zip.substring(0,5))){
-	    throw new IllegalArgumentException();
-	}
-	return zip;
+    public static String toZip(String code){
+		if (code.length() != 32 || code.charAt(0) != '|' || code.charAt(31) != '|'){
+		    throw new IllegalArgumentException();
+		}
+		code = code.substring(1,31);
+		String zip = "";
+		for (int i = 0; i < 30; i+=5){
+		    int temp = barToNum(code.substring(i,i+5));
+		    if (temp == -1){
+				throw new IllegalArgumentException();
+		    }
+		    zip += temp;
+		}
+		int checkSum = Integer.parseInt(zip.substring(5,6));
+
+		int realSum = 0; 
+		for (int i = 0; i < 5; i++){
+			realSum += Integer.parseInt(zip.substring(i,i+1));
+		}
+
+
+		if (checkSum != realSum){
+		    throw new IllegalArgumentException();
+		}
+		return zip;
     }
     
 }
